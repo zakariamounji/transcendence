ff2a9f8a33a8b58511a2002fec508ae23dae9437
AimadBifkirn <aimadbifkirn@gmail.com>
2026-06-29 16:45:22 +0100

    removed @unique from uid attribute in user model, cause @id already covers it

--- prisma/feed.ts
+++ prisma/feed.ts
@@ -2,14 +2,12 @@ import { UserRole, UserStatus } from '../generated/prisma/client';
 import { faker } from '@faker-js/faker';
 import { prisma } from 'lib/prisma';
 
-const USERS_COUNT = 20;
+const USERS_COUNT = 2;
 
 async function seedUsers() {
   console.log('Deleting users...');
 
-  await prisma.user.deleteMany({
-    
-  }) // this deletes all users in the database
+  await prisma.user.deleteMany({}); // delete all existing users
 
   const total = await prisma.user.count()
   console.log(`Done. Total users in DB: ${total}`)
