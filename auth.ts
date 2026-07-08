94dd80dc437b5f0b813989290bd994a417ab3701
Batrii <dhj.nafiai@gmail.com>
2026-07-08 18:01:56 +0100

    add google auth

--- auth.ts
+++ auth.ts
@@ -10,4 +10,11 @@ const prisma = new PrismaClient({ adapter });
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   emailAndPassword: { enabled: true },
+  baseUrl: process.env.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
