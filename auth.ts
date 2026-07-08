f20038b8493e363971b2e269b8c42d221924a77f
Batrii <dhj.nafiai@gmail.com>
2026-07-08 22:02:26 +0100

    github auth added

--- auth.ts
+++ auth.ts
@@ -18,5 +18,9 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
   },
 });
