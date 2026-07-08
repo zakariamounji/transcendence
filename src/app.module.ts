01f949e7984cf2eaef87a120291506cd78b12ad9
AimadBifkirn <aimadbifkirn@gmail.com>
2026-07-08 14:04:44 +0100

    removed user model to start the real job

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
-// import { Auth } from "./auth/auth.factory"; // mzl ghan9ad hada
 import { auth } from 'auth';
-// import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, AuthModule.forRoot({ auth }), AuthModule],
+  imports: [DatabaseModule, AuthModule.forRoot({ auth }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/dto/update-user.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from 'src/common/dto/create-register.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-}
