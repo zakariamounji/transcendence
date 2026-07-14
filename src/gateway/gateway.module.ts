8616ca1871c7d8c4d061d7b40b0fe903eb6a3a7d
AimadBifkirn <aimadbifkirn@gmail.com>
2026-07-14 15:28:14 +0100

    imported battlemodule into gatwayModule so that gatway.ts can be constructed with battle service

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MyGateway } from './gateway';
+import { BattleModule } from 'src/battle/battle.module';
 
 
 @Module({
+    imports: [BattleModule],
     providers: [MyGateway],
 })
 export class GatewayModule {}
