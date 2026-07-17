85f214a277fd64fdfa161caff64e91f5a60151a1
AimadBifkirn <aimadbifkirn@gmail.com>
2026-07-17 11:18:36 +0100

    ...

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -13,7 +13,7 @@ export const auth = betterAuth({
   emailAndPassword: { enabled: true },
   baseURL: process.env.BETTER_AUTH_URL,
   //ila zdti chi localhost b port wahad khor zido hna bach req twasal
-  trustedOrigins: ["http://localhost:3000", "http://localhost:5500", "http://localhost:8080", "http://localhost:1337"],
+  trustedOrigins: ["http://localhost:3000", "http://localhost:5500", "http://localhost:8080", "http://localhost:1337", "http://10.14.4.9:1337"],
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
