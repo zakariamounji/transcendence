850bbb00d189b84177169143fafd15a79fe5ba1f
AimadBifkirn <aimadbifkirn@gmail.com>
2026-07-18 09:45:01 +0100

    ...

--- src/battle/battle.module.ts
+++ src/battle/battle.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { DatabaseModule } from "src/database/database.module";
 import { BattleService } from "./battle.service";
 import { BattleController } from "./battle.controller";
+import { UserModule } from "src/user/user.module";
+import { UserService } from "src/user/user.service";
 
 @Module({
   imports: [DatabaseModule],
-  providers: [BattleService],
+  providers: [BattleService, UserService],
   controllers: [BattleController],
   exports: [BattleService],
 })
